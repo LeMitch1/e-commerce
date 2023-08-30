3e9ccac8035a9c31559347715d3b48cfd1db6427
Mitchell Le <lemitch@pm.me>
2023-08-30 16:17:53 -0400

    Add Register and began logic for form

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,51 @@
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  FormHelperText,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
 export default function Register() {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const handleUserNameChange = (e) => setUserName(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+  const isError = username === "";
+
+  const validPassword = password.length > 7;
+
+  // WORK ON PASSWORD
+
   return (
     <>
-      <h1>Register</h1>
+      <FormControl isInvalid={isError}>
+        <FormLabel>Username</FormLabel>
+        <Input type="text" value={username} onChange={handleUserNameChange} />
+        {!isError ? (
+          <FormHelperText>
+            Enter the username you'd like to receive the newsletter on.
+          </FormHelperText>
+        ) : (
+          <FormErrorMessage>Username is required.</FormErrorMessage>
+        )}
+      </FormControl>
+      <FormControl isInvalid={validPassword}>
+        <FormLabel>Password</FormLabel>
+        <Input
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        {!validPassword ? (
+          <FormHelperText>Enter your password</FormHelperText>
+        ) : (
+          <FormErrorMessage>
+            Password must be at least 8 characters
+          </FormErrorMessage>
+        )}
+      </FormControl>
     </>
   );
 }
