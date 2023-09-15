5e722993fbc5fe30fea67a7848b87c03a29e514e
Mitchell Le <lemitch@pm.me>
2023-09-15 18:31:44 -0400

    Add spinner to login screen as it loads

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,13 +11,15 @@ import {
   useColorModeValue,
   FormHelperText,
   FormErrorMessage,
+  Spinner,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { fetchLogin } from "../API";
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [isFetching, setIsFetching] = useState(true);
 
   const handleUsernameChange = (e) => setUsername(e.target.value);
   const handlePasswordChange = (e) => setPassword(e.target.value);
@@ -35,80 +37,92 @@ export default function Login() {
     localStorage.setItem("userToken", JSON.stringify(user.token));
   }
 
+  useEffect(() => {
+    setTimeout(() => setIsFetching(false), 250);
+  }, []);
+
   return (
-    <form method="POST" onSubmit={handleSubmit}>
-      <Flex
-        minH={"87vh"}
-        align={"center"}
-        justify={"center"}
-        bg={useColorModeValue("gray.50", "gray.800")}
-      >
-        <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-          <Stack align={"center"}>
-            <Heading fontSize={"4xl"}>Sign in to your account</Heading>
-            <Text fontSize={"lg"} color={"gray.600"}>
-              to enjoy all of our cool features
-            </Text>
-          </Stack>
-          <Box
-            rounded={"lg"}
-            bg={useColorModeValue("white", "gray.700")}
-            boxShadow={"lg"}
-            p={8}
-          >
-            <Stack spacing={4}>
-              <FormControl id="email" isInvalid={isError} isRequired>
-                <FormLabel>Username</FormLabel>
-                <Input
-                  type="text"
-                  value={username}
-                  onChange={handleUsernameChange}
-                />
-                {!isError ? (
-                  <FormHelperText>Please enter your username</FormHelperText>
-                ) : (
-                  <FormErrorMessage>Username is required.</FormErrorMessage>
-                )}
-              </FormControl>
-              <FormControl id="password" isInvalid={validPassword} isRequired>
-                <FormLabel>Password</FormLabel>
-                <Input
-                  type="password"
-                  value={password}
-                  onChange={handlePasswordChange}
-                />
-                {!validPassword ? (
-                  <FormHelperText>Enter your password</FormHelperText>
-                ) : (
-                  <FormErrorMessage>
-                    Password must be at least 8 characters
-                  </FormErrorMessage>
-                )}
-              </FormControl>
-              <Stack spacing={10}>
-                <Stack
-                  direction={{ base: "column", sm: "row" }}
-                  align={"start"}
-                  justify={"space-between"}
-                >
-                  <Text>Don&apos;t have an account?</Text>
-                  <Text color={"blue.400"}>Sign up here</Text>
-                </Stack>
-                <Button
-                  bg={"blue.400"}
-                  color={"white"}
-                  _hover={{
-                    bg: "blue.500",
-                  }}
-                  type="submit"
-                >
-                  Sign in
-                </Button>
-              </Stack>
-            </Stack>
-          </Box>
+    <>
+      {isFetching ? (
+        <Stack
+          paddingTop={12}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Spinner boxSize="12em" size="xl" color="blue.500" thickness="4px" />
         </Stack>
-      </Flex>
-    </form>
+      ) : (
+        <Flex
+          minH={"87vh"}
+          align={"center"}
+          justify={"center"}
+          bg="gray.50"
+          as="form"
+          onSubmit={handleSubmit}
+        >
+          <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+            <Stack align={"center"}>
+              <Heading fontSize={"4xl"}>Sign in to your account</Heading>
+              <Text fontSize={"lg"} color={"gray.600"}>
+                to enjoy all of our cool features
+              </Text>
+            </Stack>
+            <Box rounded={"lg"} bg="white" boxShadow={"lg"} p={8}>
+              <Stack spacing={4}>
+                <FormControl id="email" isInvalid={isError} isRequired>
+                  <FormLabel>Username</FormLabel>
+                  <Input
+                    type="text"
+                    value={username}
+                    onChange={handleUsernameChange}
+                  />
+                  {!isError ? (
+                    <FormHelperText>Please enter your username</FormHelperText>
+                  ) : (
+                    <FormErrorMessage>Username is required.</FormErrorMessage>
+                  )}
+                </FormControl>
+                <FormControl id="password" isInvalid={validPassword} isRequired>
+                  <FormLabel>Password</FormLabel>
+                  <Input
+                    type="password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                  />
+                  {!validPassword ? (
+                    <FormHelperText>Enter your password</FormHelperText>
+                  ) : (
+                    <FormErrorMessage>
+                      Password must be at least 8 characters
+                    </FormErrorMessage>
+                  )}
+                </FormControl>
+                <Stack spacing={10}>
+                  <Stack
+                    direction={{ base: "column", sm: "row" }}
+                    align={"start"}
+                    justify={"space-between"}
+                  >
+                    <Text>Don&apos;t have an account?</Text>
+                    <Text color={"blue.400"}>Sign up here</Text>
+                  </Stack>
+                  <Button
+                    bg={"blue.400"}
+                    color={"white"}
+                    _hover={{
+                      bg: "blue.500",
+                    }}
+                    type="submit"
+                  >
+                    Sign in
+                  </Button>
+                </Stack>
+              </Stack>
+            </Box>
+          </Stack>
+        </Flex>
+      )}
+    </>
   );
 }
