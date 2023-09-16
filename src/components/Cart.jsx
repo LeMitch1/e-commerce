24e58b47f4968895e1f250d894f36f4c860f6563
Mitchell Le <lemitch@pm.me>
2023-09-16 19:17:17 -0400

    Create Cart components

---
+++ src/components/Cart.jsx
@@ -0,0 +1,104 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  HStack,
+  Link,
+  Stack,
+  useColorModeValue as mode,
+} from "@chakra-ui/react";
+import CartItem from "./CartItem";
+import CartOrderSummary from "./CartOrderSummary";
+
+export const cartData = [
+  {
+    id: "1",
+    price: 109.95,
+    name: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    description:
+      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    quantity: 3,
+    imageUrl: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  },
+  {
+    id: "2",
+    price: 22.3,
+    name: "Mens Casual Premium Slim Fit T-Shirts",
+    description:
+      "Slim-fitting style, contrast raglan long sleeve, three-button henley placket",
+    quantity: 3,
+    imageUrl:
+      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+  },
+  {
+    id: "3",
+    price: 55.99,
+    name: "Mens Cotton Jacket",
+    description:
+      "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions",
+    quantity: 3,
+    imageUrl: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+  },
+];
+
+export default function Cart() {
+  return (
+    <Box
+      maxW={{
+        base: "3xl",
+        lg: "7xl",
+      }}
+      mx="auto"
+      px={{
+        base: "4",
+        md: "8",
+        lg: "12",
+      }}
+      py={{
+        base: "6",
+        md: "8",
+        lg: "20vh",
+      }}
+    >
+      <Stack
+        direction={{
+          base: "column",
+          lg: "row",
+        }}
+        align={{
+          lg: "flex-start",
+        }}
+        spacing={{
+          base: "8",
+          md: "16",
+        }}
+      >
+        <Stack
+          spacing={{
+            base: "8",
+            md: "10",
+          }}
+          flex="2"
+        >
+          <Heading fontSize="2xl" fontWeight="extrabold">
+            Shopping Cart (3 items)
+          </Heading>
+
+          <Stack spacing="6">
+            {cartData.map((item) => (
+              <CartItem key={item.id} {...item} />
+            ))}
+          </Stack>
+        </Stack>
+
+        <Flex direction="column" align="center" flex="1">
+          <CartOrderSummary />
+          <HStack mt="6" fontWeight="semibold">
+            <p>or</p>
+            <Link color={mode("blue.500", "blue.200")}>Continue shopping</Link>
+          </HStack>
+        </Flex>
+      </Stack>
+    </Box>
+  );
+}
